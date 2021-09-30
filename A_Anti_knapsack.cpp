dca2fdd8b35608aa08d83e5f8bfdedd4a6b91c1d
Shambhav Goswami <56958751+shambhavgo@users.noreply.github.com>
2021-10-01 00:01:55 +0530

    Add files via upload

---
+++ A_Anti_knapsack.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;cin>>t;
+    while(t--)
+    {
+        long long n,k;cin>>n>>k;
+        if(n<=k)cout<<0<<endl;
+        else
+        ()
+    }
+    return 0;
+}
